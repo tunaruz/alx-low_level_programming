c8c53e348dabf2983195aedc30cf7321bceb33bf
assya ouadi <assyaouadi@gmail.com>
2023-04-20 09:29:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - Entry point
  *Return: Always 0(Success)
@@ -8,4 +9,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	    return (0);
+
 }
