a575820a6cd0cfc484639db8a384c5872ca694a2
assya ouadi <assyaouadi@gmail.com>
2023-04-21 07:42:58 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - printf the alphabetic
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
+
