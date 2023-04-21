ec325f0254be2f3723d37843c52c3e58103f0cae
assya ouadi <assyaouadi@gmail.com>
2023-04-21 08:08:39 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+* Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+
+}
