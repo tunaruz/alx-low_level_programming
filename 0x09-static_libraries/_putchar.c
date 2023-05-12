db9f03c08d6e133fd222e4ae5738ab52f5674682
assya ouadi <assyaouadi@gmail.com>
2023-05-12 08:26:44 -0700

    update

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * Return: success 1
+ * error -1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return putchar(c);
+}
